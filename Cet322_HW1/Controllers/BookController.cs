4e05aff00ffce991ea8ef625052c20c1284b1064
Monster <Monster@DESKTOP-V3KNKVD>
2024-02-28 02:54:22 +0300

    Add project files.

---
+++ Cet322_HW1/Controllers/BookController.cs
@@ -0,0 +1,21 @@
+﻿using Cet322_HW1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cet322_HW1.Controllers
+{
+    public class BookController : Controller
+    {
+        public IActionResult Display(string bookName, string author, int pageNumber, DateTime dueDate)
+        {
+            var book = new Book
+            {
+                BookName = bookName,
+                Author = author,
+                PageNumber = pageNumber,
+                DueDate = dueDate
+            };
+
+            return View(book);
+        }
+    }
+}
